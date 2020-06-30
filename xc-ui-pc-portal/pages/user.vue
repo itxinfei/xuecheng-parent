dcd899df469e19bc9af67eeaaadb750f20b89a1d
心飞为你飞 <747011882@qq.com>
2020-06-30 20:06:33 +0800

    新增前端工程

---
+++ xc-ui-pc-portal/pages/user.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    user用户管理页面<nuxt-link :to="'/user/101'">修改用户</nuxt-link>
+    <nuxt-child/>
+  </div>
+</template>
+<script>
+  export default {
+    layout:'test'
+  }
+</script>
+<style>
+
+</style>
---
+++ xc-ui-pc-portal/pages/user/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    修改用户信息{{id}}，姓名{{name}}
+  </div>
+</template>
+<script>
+  export default {
+    layout:'test',
+    asyncData(){
+      return{
+        name:'黄礼缘'
+      }
+    },
+    data(){
+      return {
+        id : ''
+      }
+    },
+    mounted(){
+      this.id = this.$route.params.id;
+    }
+  }
+</script>
+<style>
+
+</style>
